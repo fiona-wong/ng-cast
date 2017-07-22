610c2558135702e908e61a8f2f4471364562c12b
Hack Reactor Student <student@hackreactor.com>
2017-07-22 15:46:50 -0700

    completed ng-cast, implement live search and starting search

--- src/components/app.js
+++ src/components/app.js
@@ -5,8 +5,13 @@ angular.module('video-player')
   controller: function(youTube) {
     this.videos = exampleVideoData;
     this.currentVideo = exampleVideoData[0];
-    this.selectVideo = (video) => {
+    this.start = () => {
+      youTube.search('cats', (response) => {
+        this.videos = response.items;
+        this.currentVideo = response.items[0];
+      });
     };
+    this.start();
     this.searchResults = (query) => {
       youTube.search(query, (response) => {
         this.videos = response.items;
