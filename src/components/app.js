9325f6ba88b45f325e451ba0e75e42d0b5403d60
Hack Reactor Student <student@hackreactor.com>
2017-07-21 20:13:54 -0700

    implement videoplayer, passed lots of tests

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,12 @@ angular.module('video-player')
   templateUrl: 'src/templates/app.html',
   controller: function() {
     this.videos = exampleVideoData,
-    this.currentVideo;
+    this.currentVideo = exampleVideoData[0];
+    this.selectVideo = (video) => {
+    };
+    this.searchResults = () => {
+      
+    };
     this.onClick = (video) => {
       console.log('click', this);
       this.currentVideo = video;
